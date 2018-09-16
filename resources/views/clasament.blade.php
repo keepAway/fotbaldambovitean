cf32e6746755964182c6b3731dad928a800b0204
keepAway <un.nenorocit@gmail.com>
2018-09-16 21:17:53 +0300

    Fixed merge issues

--- resources/views/clasament.blade.php
+++ resources/views/clasament.blade.php
@@ -47,11 +47,11 @@
         </div>
         <div class="col-lg-7 col-md-12 col-sm-12" style="background: #fff; box-shadow: 0 1px 3px rgba(0,0,0,0.15);">
             <div class="row">
-                <div class="col-sm-12 font-weight-bold" style="height: 40px; margin-top: 20px; margin-bottom: 5px; font-size: 20px;">
+                <div class="col-sm-12 font-weight-bold" style="height: 40px; margin-top: 20px; margin-bottom: 5px;font-size: 20px;">
                     Liga {{$liga}} {{isset($seria) && $seria != NULL ? ' | Seria ' . $seria : ''}}
                     <label style="font-size: 16px; margin-left: 5px;"> Clasament</label>
                 </div>
-                <div class="col-sm-12" style="padding-right: 0px; padding-left: 0px;">
+                <div class="col-sm-12" style="padding-left: 5px; padding-right: 5px;">
                     <ul class="nav nav-tabs font-weight-bold" style="background-color: #085f00; color: #fff; border: 1px solid #085f00; border-radius: 0;">
                         <li class="nav-item">
                             <a class="nav-link" style="border-radius: 0; background-color: {{$tab == NULL ? '#053600' : ''}}" href="{{ route('clasament', ['liga' => $liga, 'seria' => $seria]) }}">Total</a>
@@ -63,7 +63,7 @@
                             <a class="nav-link" style="border-radius: 0; background-color: {{$tab == '2' ? '#053600' : ''}}" href="{{ route('clasament', ['liga' => $liga, 'seria' => $seria, 'tab'=> 2]) }}">Deplasare</a>
                         </li>
                         <li class="nav-item d-block d-sm-none">
-                            <a class="nav-link" style="border-radius: 0; background-color: {{$tab == '3' ? '#053600' : ''}}" href="{{ route('clasament', ['liga' => 1, 'tab'=> 3]) }}">Forma</a>
+                            <a class="nav-link" style="border-radius: 0; background-color: {{$tab == '3' ? '#053600' : ''}}" href="{{ route('clasament', ['liga' => $liga, 'seria' => $seria, 'tab'=> 3]) }}">Forma</a>
                         </li>
                     </ul>
                     <div class="table-responsive-sm table-responsive-md">
@@ -88,14 +88,33 @@
                                 <tr>
                                     <td class="font-weight-bold" style="width: 1%; background: #{{$i <= 2 ? '004682' : '000'}}; text-align: center; color: #fff;">{{$i++}}.</td>
                                     <td class="text-left font-weight-bold">{{$echipa->echipa}}</td>
-                                    <td class="{{$tab == 3 ? 'd-none' : ''}}">{{$echipa->meciuri}}</td>
-                                    <td class="{{$tab == 3 ? 'd-none' : ''}}">{{$echipa->victorii}}</td>
-                                    <td class="d-none d-sm-table-cell">{{$echipa->egaluri}}</td>
-                                    <td class="d-none d-sm-table-cell">{{$echipa->infrangeri}}</td>
-                                    <td class="text-nowrap{{$tab == 3 ? ' d-none' : ''}}">{{$echipa->marcate}} - {{$echipa->primite}}</td>
-                                    <td class="d-none d-sm-table-cell">{{$echipa->golaveraj}}</td>
-                                    <td class="{{$tab == 3 ? 'd-none' : ''}}">{{$echipa->puncte}}</td>
+                                    <td class="{{$tab == 3 ? 'd-none' : ''}}">
+                                        {{$tab == NULL ? $echipa->t_meciuri : ($tab == 1 ? $echipa->a_meciuri : $echipa->d_meciuri)}}
+                                    </td>
+                                    <td class="{{$tab == 3 ? 'd-none' : ''}}">
+                                        {{$tab == NULL ? $echipa->t_victorii : ($tab == 1 ? $echipa->a_victorii : $echipa->d_victorii)}}
+                                    </td>
+                                    <td class="d-none d-sm-table-cell">
+                                        {{$tab == NULL ? $echipa->t_egaluri : ($tab == 1 ? $echipa->a_egaluri : $echipa->d_egaluri)}}
+                                    </td>
+                                    <td class="d-none d-sm-table-cell">
+                                        {{$tab == NULL ? $echipa->t_infrangeri : ($tab == 1 ? $echipa->a_infrangeri : $echipa->d_infrangeri)}}
+                                    </td>
+                                    <td class="text-nowrap{{$tab == 3 ? ' d-none' : ''}}">
+                                        {{$tab == NULL ? $echipa->t_marcate : ($tab == 1 ? $echipa->a_marcate : $echipa->d_marcate)}}
+                                         - 
+                                        {{$tab == NULL ? $echipa->t_primite : ($tab == 1 ? $echipa->a_primite : $echipa->d_primite)}}
+                                    </td>
+                                    <td class="d-none d-sm-table-cell">
+                                        {{$tab == NULL ? $echipa->t_golaveraj : ($tab == 1 ? $echipa->a_golaveraj : $echipa->d_golaveraj)}}
+                                    </td>
+                                    <td class="{{$tab == 3 ? 'd-none' : ''}}">
+                                        {{$tab == NULL ? $echipa->t_puncte : ($tab == 1 ? $echipa->a_puncte : $echipa->d_puncte)}}
+                                    </td>
                                     <td class="{{$tab == 3 ? 'd-inline-flex' : 'd-none d-sm-inline-flex'}}">
+                                        <div class="forma" style="background: #656565; cursor: pointer;" data-toggle="tooltip" data-placement="top" title="Etapa urmatoare: ">
+                                            ?
+                                        </div>
                                         @foreach($echipa->forma as $forma)
                                             @if($forma->forma == 'V')
                                                 <div class="forma" style="background: #28a745; cursor: pointer;" data-toggle="tooltip" data-placement="top" title="{{$forma->g_gazde}} : {{$forma->g_oaspeti}} ({{$forma->gazde}} - {{$forma->oaspeti}})">
@@ -105,22 +124,12 @@
                                                 <div class="forma" style="background: #FFA500; cursor: pointer;" data-toggle="tooltip" data-placement="top" title="{{$forma->g_gazde}} : {{$forma->g_oaspeti}} ({{$forma->gazde}} - {{$forma->oaspeti}})">
                                                     {{$forma->forma}}
                                                 </div>
-                                            @foreach($echipa->forma as $forma)
-                                                @if($forma->forma == 'V')
-                                                    <div class="forma" style="background: #28a745; cursor: pointer;" data-toggle="tooltip" data-placement="top" title="{{$forma->g_gazde}} : {{$forma->g_oaspeti}} ({{$forma->gazde}} - {{$forma->oaspeti}})">
-                                                        {{$forma->forma}}
-                                                    </div>
-                                                @elseif($forma->forma == 'E')
-                                                    <div class="forma" style="background: #FFA500; cursor: pointer;" data-toggle="tooltip" data-placement="top" title="{{$forma->g_gazde}} : {{$forma->g_oaspeti}} ({{$forma->gazde}} - {{$forma->oaspeti}})">
-                                                        {{$forma->forma}}
-                                                    </div>
-                                                @else
-                                                    <div class="forma" style="background: #DB4727; cursor: pointer;" data-toggle="tooltip" data-placement="top" title="{{$forma->g_gazde}} : {{$forma->g_oaspeti}} ({{$forma->gazde}} - {{$forma->oaspeti}})">
-                                                        î
-                                                    </div>
-                                                @endif
-                                            @endforeach
-                                        </div>
+                                            @else
+                                                <div class="forma" style="background: #DB4727; cursor: pointer;" data-toggle="tooltip" data-placement="top" title="{{$forma->g_gazde}} : {{$forma->g_oaspeti}} ({{$forma->gazde}} - {{$forma->oaspeti}})">
+                                                    î
+                                                </div>
+                                            @endif
+                                        @endforeach
                                     </td>
                                 </tr>
                                 @endforeach
