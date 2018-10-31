c9b7104a6c38870565a60397791ffecf49d8f85c
keepAway <un.nenorocit@gmail.com>
2018-10-31 22:03:59 +0200

    Seeders update

---
+++ database/seeds/liga_3_5.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class liga_3_5 extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
