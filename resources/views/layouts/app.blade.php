60057986fb3c358dad9898c4136664e9444f3111
keepAway <un.nenorocit@gmail.com>
2018-11-04 21:13:23 +0200

    header fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         </title>
         
         <link rel="stylesheet" href="{{ asset('css/app.css?v=2.1') }}">
-        <link rel="stylesheet" href="{{ asset('css/style.css?v=1.5') }}">
+        <link rel="stylesheet" href="{{ asset('css/style.css?v=2.1') }}">
         <link rel="stylesheet" href="{{ asset('css/bootstrap-social.css') }}">
         <link rel="stylesheet" href="{{ asset('css/jquery.mCustomScrollbar.min.css') }}">
         <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
