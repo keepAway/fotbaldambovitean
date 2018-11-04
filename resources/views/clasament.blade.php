c29eaaec986f012300f27bb9390b17c34f516075
keepAway <un.nenorocit@gmail.com>
2018-11-04 22:56:12 +0200

    fb comments plugin

--- resources/views/clasament.blade.php
+++ resources/views/clasament.blade.php
@@ -413,6 +413,16 @@
                 <div class="col-sm-12 font-weight-bold" style="height: 40px; margin-top: 20px; margin-bottom: 5px; font-size: 20px;">
                     <label style="font-size: 16px; margin-left: 5px;"> Comenteaza etapa</label>
                 </div>
+                <div id="fb-root"></div>
+                <script>
+                    (function(d, s, id) {
+                        var js, fjs = d.getElementsByTagName(s)[0];
+                        if (d.getElementById(id)) return;
+                        js = d.createElement(s); js.id = id;
+                        js.src = 'https://connect.facebook.net/ro_RO/sdk.js#xfbml=1&version=v3.1&appId=2136989686622565&autoLogAppEvents=1';
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'facebook-jssdk'));
+                </script>
                 <div class="col-sm-12">
                     <div class="fb-comments" data-href="{{$current_url}}" data-numposts="5" data-width="100%"></div>
                 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,14 +14,14 @@
 
         <script>
             window.fbAsyncInit = function() {
-                FB.init({
-                  appId      : '2136989686622565',
-                  cookie     : true,
-                  xfbml      : true,
-                  version    : 'v3.2'
-                });
-                  
                 FB.AppEvents.logPageView();
+                (function(d, s, id) {
+                  var js, fjs = d.getElementsByTagName(s)[0];
+                  if (d.getElementById(id)) return;
+                  js = d.createElement(s); js.id = id;
+                  js.src = 'https://connect.facebook.net/ro_RO/sdk.js#xfbml=1&version=v3.2&appId=2136989686622565&autoLogAppEvents=1';
+                  fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
             };
         </script>
 
