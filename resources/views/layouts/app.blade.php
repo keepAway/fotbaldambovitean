4cb040be39f2d39c8893f7fdbc105c1658c26fe2
keepAway <un.nenorocit@gmail.com>
2018-11-16 01:11:22 +0200

    adding another sense

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,12 @@
         <title>@yield('title')</title>
         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <script>
-          (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "ca-pub-4051259731437503",
-            enable_page_level_ads: true
-          });
+             (adsbygoogle = window.adsbygoogle || []).push({
+                  google_ad_client: "ca-pub-7606412229703320",
+                  enable_page_level_ads: true
+             });
         </script>
+
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-128595058-1"></script>
         <script>
