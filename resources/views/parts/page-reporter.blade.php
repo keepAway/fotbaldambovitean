6bf6df5fd42c66db27b081efe409660665f98afc
ionut <catana.ionut@gmail.com>
2019-02-11 21:42:01 +0200

    I made Stiati ca...

--- resources/views/parts/page-reporter.blade.php
+++ resources/views/parts/page-reporter.blade.php
@@ -1,9 +1,16 @@
-<div class="text-center" style="background-color: #323b3e">
+<div class="text-center mb-2" style="background-color: #323b3e; position: relative;">
+	<div class="pt-2 pr-2" style="position: absolute; top: 0; right: 0; color: #f4d90c"><em>{{date('d-m-Y')}}</em></div>
+	<div class="pt-3 pb-3 pl-2 pr-2">
+		<strong><h6 style="color: #f4d90c">Știați că...</h6></strong>
+		<p style="color: #fff;">Înfiinţat pe 11 februarie 1916, Sportul Studenţesc rămâne unul dintre cluburile de mare tradiţie ale fotbalului românesc. Primul preşedinte al grupării nou înfiinţate a fost reputatul matematician Traian Lălescu. Unul dintre oamenii care au ajutat la fondarea acestei echipe a fost Octav Luchide, secretar general al Federaţiei de Fotbal din România (între 1930-1942), organizatorul sistemului divizionar ca formulă de desfăşurare a campionatului naţional de fotbal.</p>
+	</div>
+</div>
+<div class="text-center mb-2" style="background-color: #323b3e">
 	<div class="pt-3 pb-3 pl-2 pr-2">
 		<strong><h6 style="color: #f4d90c">Fii reporter fotbal-arena.ro!</h6></strong>
-		<p>Vrei să te faci cunoscut? Noi te facem mare în fotbalul mic.</p>
-		<p>Ai aflat un zvon? Ai informații despre o echipă, un jucător sau un eveniment din fotbalul dâmbovițean?</p>
-		<p>Trimite-ne tot ce crezi că ar vrea şi alţii să vadă la adresa <a href="mailto:contact@fotbal-arena.ro" style="color: #f4d90c">contact@fotbal-arena.ro</a> şi noi vom publica pe site, sub semnătura ta!</p>
-		<p>Tot ce mişcă în fotbalul dâmbovițean este pe fotbal-arena.ro!</p>
+		<p style="color: #fff;">Vrei să te faci cunoscut? Noi te facem mare în fotbalul mic.</p>
+		<p style="color: #fff;">Ai aflat un zvon? Ai informații despre o echipă, un jucător sau un eveniment din fotbalul dâmbovițean?</p>
+		<p style="color: #fff;">Trimite-ne tot ce crezi că ar vrea şi alţii să vadă la adresa <a href="mailto:contact@fotbal-arena.ro" style="color: #f4d90c">contact@fotbal-arena.ro</a> şi noi vom publica pe site, sub semnătura ta!</p>
+		<p style="color: #fff;">Tot ce mişcă în fotbalul dâmbovițean este pe fotbal-arena.ro!</p>
 	</div>
 </div>
